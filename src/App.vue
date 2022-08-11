979b272c7bccb5cac6032c4edfce3c0d4051708b
Ayibatari Ibaba <tariibabadev@gmail.com>
2022-08-11 12:56:01 +0100

    Enable countdown

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <AppTimer :elapsed="0" :limit="10" />
+  <AppTimer
+    :elapsed="timeElapsed"
+    :limit="timeLimit"
+  />
 </template>
 
 <script>
@@ -7,6 +10,27 @@ import AppTimer from './components/AppTimer.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      timeElapsed: 0,
+      timerInterval: undefined,
+      timeLimit: 10,
+    };
+  },
+  methods: {
+    startTimer() {
+      this.timerInterval = setInterval(() => {
+        // Stop counting when there is no more time left
+        if (++this.timeElapsed === this.timeLimit) {
+          clearInterval(this.timerInterval);
+        }
+      }, 1000);
+    },
+  },
+  // Start timer immediately
+  mounted() {
+    this.startTimer();
+  },
   components: {
     AppTimer,
   },
